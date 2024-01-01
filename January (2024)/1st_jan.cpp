9dc42ee2c332b43ef8977856d219a938059e1d86
Beast-Alok <alokraghuwanshi2004@gmail.com>
2024-01-01 17:16:35 +0530

    1st jan solution

---
+++ January (2024)/1st_jan.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// GFG solution from here
+class Solution {
+  public:
+    bool canPair(vector<int> nums, int k) {
+        // Code here.
+        map <int, int> f;
+        
+        for(int i = 0; i<nums.size(); i++){
+            int current_value = nums[i] % k;
+            int needed_value = (k - current_value) % k;
+            
+            if(f.count(needed_value)){
+                --f[needed_value];
+                
+                if(f[needed_value] == 0){
+                    f.erase(needed_value);
+                }
+            }else{
+                ++f[current_value];
+            }
+        }
+        
+        return f.size() == 0;
+    }
+};
+
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n, k;
+        cin >> n >> k;
+        vector<int> nums(n);
+        for (int i = 0; i < nums.size(); i++) cin >> nums[i];
+        Solution ob;
+        bool ans = ob.canPair(nums, k);
+        if (ans)
+            cout << "True\n";
+        else
+            cout << "False\n";
+    }
+    return 0;
+}
+
+//code written by alok kumar raghuwanhsi
