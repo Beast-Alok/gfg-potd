3510219e790973f18f24fca7e7882b62d66f4041
Alok Kumar Raghuwanshi <143905515+Beast-Alok@users.noreply.github.com>
2024-05-26 09:32:49 +0530

    Create May_26th.cpp

---
+++ May (2024)/May_26th.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+
+  public:
+    int findMinCost(string x, string y, int costX, int costY) {
+        // Your code goes here
+        int n = x.size(), m = y.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(x[i-1] == y[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        
+        return ((n-dp[n][m]) * costX) + ((m-dp[n][m]) * costY);
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string x, y;
+        cin >> x >> y;
+
+        int costX, costY;
+        cin >> costX >> costY;
+
+        Solution ob;
+        cout << ob.findMinCost(x, y, costX, costY);
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
