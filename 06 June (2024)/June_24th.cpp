e90417a34895c2ee29f48286d7cd064603e2a5f5
Alok Kumar Raghuwanshi <143905515+Beast-Alok@users.noreply.github.com>
2024-06-24 11:00:27 +0530

    Create June_24th.cpp

---
+++ 06 June (2024)/June_24th.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    long long sumMatrix(long long n, long long q) {
+        if(q > n*2) return 0;
+        return q <= n+1 ? q-1 : n*2 - q + 1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n,q;
+        
+        cin>>n>>q;
+
+        Solution ob;
+        cout << ob.sumMatrix(n,q) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
