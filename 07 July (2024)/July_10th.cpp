7eeee4c36b068b514b6e3a3e3797fc21e561fa43
Alok Kumar Raghuwanshi <143905515+Beast-Alok@users.noreply.github.com>
2024-07-10 01:44:53 +0530

    Update July_10th.cpp

--- 07 July (2024)/July_10th.cpp
+++ 07 July (2024)/July_10th.cpp
@@ -1,1 +1,47 @@
+//{ Driver Code Starts
+// Initial Template for C++
 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int maxSquare(int n, int m, vector<vector<int>> mat) {
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(mat[i-1][j-1] == 1){
+                    dp[i][j]=1+min({dp[i-1][j-1],dp[i-1][j],dp[i][j-1]});
+                }
+            }
+        }
+        int ans=0;
+        for(auto x : dp){
+            ans=max(ans,*max_element(x.begin(),x.end()));
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> mat(n, vector<int>(m, 0));
+        for (int i = 0; i < n * m; i++)
+            cin >> mat[i / m][i % m];
+
+        Solution ob;
+        cout << ob.maxSquare(n, m, mat) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
