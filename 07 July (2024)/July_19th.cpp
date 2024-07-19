e983780385eb533215f00d70d9dc08ad8c70697a
Alok Kumar Raghuwanshi <143905515+Beast-Alok@users.noreply.github.com>
2024-07-19 18:25:44 +0530

    Create July_19th.cpp

---
+++ 07 July (2024)/July_19th.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+//  User function template for C++
+
+class Solution {
+  public:
+    vector<int> constructLowerArray(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        vector <int> ans;
+        vector <int> temp = arr;
+        sort(temp.begin(),temp.end());
+        for(int i=0;i<n;i++){
+            int index = lower_bound(temp.begin(),temp.end(),arr[i])-temp.begin();
+            ans.push_back(index);
+            temp.erase(temp.begin()+index);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int n = arr.size();
+        Solution ob;
+        vector<int> a = ob.constructLowerArray(arr);
+        for (int i = 0; i < n; i++) {
+            cout << a[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
