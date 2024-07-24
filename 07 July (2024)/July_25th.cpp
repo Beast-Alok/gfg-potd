56d6f2d073767b9ecc58d0ef1748685c32b6467b
Alok Kumar Raghuwanshi <143905515+Beast-Alok@users.noreply.github.com>
2024-07-25 04:29:09 +0530

    Create July_25th.cpp

---
+++ 07 July (2024)/July_25th.cpp
@@ -0,0 +1,128 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val)
+        : data(val)
+        , left(nullptr)
+        , right(nullptr) {}
+};
+
+bool help(Node* root, long long int up, long long int l) {
+    if (root->data >= up || root->data <= l)
+        return false;
+    bool ans = true;
+    if (root->left)
+        ans = help(root->left, root->data, l);
+    if (ans && root->right)
+        ans = help(root->right, up, root->data);
+    return ans;
+}
+
+bool isValidBST(Node* root) {
+    return help(root, 3147483648, -3147483649);
+}
+
+int Height(Node* root) {
+
+    if (root == NULL)
+        return 0;
+
+    int leftHeight = Height(root->left);
+
+    int rightHight = Height(root->right);
+
+    if (leftHeight == -1 || rightHight == -1 || abs(leftHeight - rightHight) > 1)
+        return -1;
+
+    return max(leftHeight, rightHight) + 1;
+}
+
+bool isBalanced(Node* root) {
+
+    if (root == NULL)
+        return true;
+
+    if (Height(root) == -1)
+        return false;
+    return true;
+}
+
+
+// } Driver Code Ends
+/*
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val)
+        : data(val)
+        , left(nullptr)
+        , right(nullptr) {}
+};
+*/
+
+class Solution {
+  public:
+    Node* solve(Node* root,int low,int high,vector<int>& nums){
+      if(low>high) return NULL;
+      int mid=low+(high-low)/2;
+      Node* temp=new Node(nums[mid]);
+      if(temp->data<root->data)
+          root->left=temp;
+      else
+          root->right=temp;
+      solve(temp,low,mid-1,nums);
+      solve(temp,mid+1,high,nums);
+      return temp;
+  }
+    Node* sortedArrayToBST(vector<int>& nums) {
+        int n=nums.size();
+        int i=0,j=n-1;
+        int mid=i+(j-i)/2;
+        Node* root=new Node(nums[mid]);
+        root->left=solve(root,0,mid-1,nums);
+        root->right=solve(root,mid+1,j,nums);
+        return root;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        Node* a = ob.sortedArrayToBST(arr);
+        if (!isValidBST(a)) {
+            cout << "false" << endl;
+            return 0;
+        }
+        bool f = isBalanced(a);
+
+        if (f)
+            cout << "true";
+        else
+            cout << "false";
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
