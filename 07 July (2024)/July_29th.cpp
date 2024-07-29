686476b69549c8c9b3b6a95609fa6d33eeb1c8db
Alok Kumar Raghuwanshi <143905515+Beast-Alok@users.noreply.github.com>
2024-07-29 13:16:30 +0530

    Create July_29th.cpp

---
+++ 07 July (2024)/July_29th.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    int rowWithMax1s(vector<vector<int> > &arr) {
+        int sum;
+        int i=0;
+        int max=0;
+        int maxI=0;
+        int count=0;
+        int n=arr.size();
+        int m=arr[0].size();
+        while(i<n){
+            sum=0;
+            for(int j=0;j<m;j++){
+                sum +=arr[i][j];
+            }
+            if(sum>max) {maxI=i;
+                max=sum;
+            }
+           // if(sum==max) count++;
+            i++;
+            
+        }
+        if(max==0) return -1;
+        return maxI;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int> > arr(n, vector<int>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> arr[i][j];
+            }
+        }
+        Solution ob;
+        auto ans = ob.rowWithMax1s(arr);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
