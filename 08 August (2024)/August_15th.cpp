150ce9964a1f704a84f711807f605cd89d3d6c98
Alok Kumar Raghuwanshi <143905515+Beast-Alok@users.noreply.github.com>
2024-08-15 19:12:56 +0530

    Create August_15th.cpp

---
+++ 08 August (2024)/August_15th.cpp
@@ -0,0 +1,114 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* node) {
+    while (node != NULL) {
+        cout << node->data % 10;
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+
+// } Driver Code Ends
+// User function template for C++
+
+/*
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution {
+  public:
+    Node* reverse(Node* head){
+        Node* prev=NULL;
+        Node* curr=head;
+        Node* next=NULL;
+        while(curr!=NULL){
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }a
+        return prev;
+    }
+    Node* addOne(Node* head) {
+        head=reverse(head);
+        Node* curr=head;
+        curr->data+=1;
+        while(curr!=NULL){
+            if(curr->data==10){
+                if(curr->next!=NULL){
+                    curr->data%=10;
+                    curr->next->data+=1;
+                }else{
+                    Node* newNode=new Node(1);
+                    curr->next=newNode;
+                }
+            }
+            curr=curr->next;
+        }
+        return reverse(head);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        if (arr.empty()) {
+            cout << -1 << endl;
+            continue;
+        }
+
+        int data = arr[0];
+        struct Node* head = new Node(data);
+        struct Node* tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            data = arr[i];
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        Solution ob;
+        head = ob.addOne(head);
+        printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
