262fc02091e045a9fcf75db76063872db72189b1
Alok Kumar Raghuwanshi <143905515+Beast-Alok@users.noreply.github.com>
2024-08-17 17:51:48 +0530

    Create August_17th.cpp

---
+++ 08 August (2024)/August_17th.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    // nums: given vector
+    // return the Product vector P that hold product except self at each index
+    vector<long long int> productExceptSelf(vector<long long int>& nums) {
+
+        // code here
+         long long int n=nums.size();
+        vector<long long int>suffix(n);
+        long long int prod=1;
+        prod=1;
+        for(int i=n-1;i>=0;i--){
+            suffix[i]=prod;
+            prod=prod*nums[i];
+        }
+        prod=1;
+        vector<long long int>ans(n);
+        for(int i=0;i<n;i++){
+            ans[i]=(prod*suffix[i]);
+            prod=prod*nums[i];
+            
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t; // number of test cases
+    cin >> t;
+    while (t--) {
+        int n; // size of the array
+        cin >> n;
+        vector<long long int> arr(n), vec(n);
+
+        for (int i = 0; i < n; i++) // input the array
+        {
+            cin >> arr[i];
+        }
+        Solution obj;
+        vec = obj.productExceptSelf(arr); // function call
+
+        for (int i = 0; i < n; i++) // print the output
+        {
+            cout << vec[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
