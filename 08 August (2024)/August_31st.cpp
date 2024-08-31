c6d2fb486f425cb4e0b79c3dd533b399bcd19988
Alok Kumar Raghuwanshi <143905515+Beast-Alok@users.noreply.github.com>
2024-08-31 14:21:45 +0530

    Create August_31st.cpp

---
+++ 08 August (2024)/August_31st.cpp
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool isSubSequence(vector<int> &v1, vector<int> &v2) {
+    int m = v2.size();
+    int n = v1.size();
+    int j = 0; // For index of v2
+
+    // Traverse v1 and v2
+    for (int i = 0; i < n && j < m; i++) {
+        if (v1[i] == v2[j]) {
+            j++;
+        }
+    }
+    return (j == m);
+}
+
+
+// } Driver Code Ends
+// Function to find three numbers in the given array
+// such that arr[smaller[i]] < arr[i] < arr[greater[i]]
+
+class Solution {
+  public:
+    vector<int> find3Numbers(vector<int> &arr) {
+        vector<int> ans;
+        int n = arr.size();
+        vector<int> isSmaller (n, -1);
+        int minE = arr[0];
+        for(int i = 1; i < n; i++){
+            if(minE < arr[i]){
+                isSmaller[i] = minE;
+            }else{
+                minE = arr[i];
+            }
+        }
+        int maxE = arr[n - 1];
+        for(int i = n - 2; i >= 0 ; i--){
+            if(maxE > arr[i]){
+                if(isSmaller[i] != -1){
+                    ans.push_back(isSmaller[i]);
+                    ans.push_back(arr[i]);
+                    ans.push_back(maxE);
+                    return ans;
+                
+                }
+            }else{
+                maxE = arr[i];
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver program to test above function
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int n = arr.size();
+        Solution obj;
+        auto res = obj.find3Numbers(arr);
+        // wrong format output
+        if (!res.empty() and res.size() != 3) {
+            cout << -1 << "\n";
+        }
+
+        if (res.empty()) {
+            cout << 0 << "\n";
+        } else if ((res[0] < res[1] and res[1] < res[2]) and isSubSequence(arr, res)) {
+            cout << 1 << "\n";
+        } else {
+            cout << -1 << "\n";
+        }
+    }
+
+    return 0;
+}
+// } Driver Code Ends
