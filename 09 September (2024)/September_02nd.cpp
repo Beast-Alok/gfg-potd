fbe66882baaafa81dafd0df82edf056d174ac562
Alok Kumar Raghuwanshi <143905515+Beast-Alok@users.noreply.github.com>
2024-09-02 22:13:13 +0530

    Create September_02nd.cpp

---
+++ 09 September (2024)/September_02nd.cpp
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class comp{
+    public:
+  bool operator()(pair<int,pair<int,int>>&a,pair<int,pair<int,int>>&b){
+      if(a.first>b.first)return true;
+      return false;
+      
+  }  
+};
+class Solution
+{
+    public:
+    //Function to return the minimum cost to react at bottom
+	//right cell from top left cell.
+    int minimumCostPath(vector<vector<int>>& grid) 
+    {
+        // Code here
+        int n=grid.size();
+        //Maintaining visted array
+        vector<vector<int>>vis(n,vector<int>(n,0));
+        //To find the element where we can reach with least points.
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,comp>pq;
+        pq.push({grid[0][0],{0,0}});
+        vis[0][0]=1;
+        //For checking all the neighbours of given element.
+        int delx[]= {1,0,-1,0};
+        int dely[]= {0,1,0,-1};
+        int x,y,w;
+        while(!pq.empty()){
+            w=pq.top().first;
+            x=pq.top().second.first;
+            y=pq.top().second.second;
+            //If bottom right element found break
+            if(x==n-1&&y==n-1)break;
+            pq.pop();
+            for(int j=0;j<4;j++)
+            {
+                int tx=x+delx[j];
+                int ty=y+dely[j];
+                if(tx>=0&&tx<n&&ty>=0&&ty<n&&vis[tx][ty]==0)
+                {
+                    //If element is not yet visited insert it in pq.
+                    pq.push({w+grid[tx][ty],{tx,ty}});
+                    vis[tx][ty]=1;
+                }
+            }
+        }
+        return w;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<vector<int>>grid(n, vector<int>(n, -1));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				cin >> grid[i][j];
+			}
+		}
+		Solution obj;
+		int ans = obj.minimumCostPath(grid);
+		cout << ans << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
