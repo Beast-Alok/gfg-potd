38525c13e215de007efea315c921bb0a36164353
Alok Kumar Raghuwanshi <143905515+Beast-Alok@users.noreply.github.com>
2024-09-18 00:12:12 +0530

    Create September_06th.cpp

---
+++ 09 September (2024)/September_06th.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    // Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(vector<int> &arr) {
+        // code here...
+        int n = arr.size();
+        int maxh =0, maxf=arr[0];
+        for (int i = 0; i < n; i++){
+            maxh += arr[i];
+            if (maxf < maxh)
+                maxf = maxh;
+                
+            if(maxh<0)
+            maxh = 0;
+
+        }
+        return maxf;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // To discard any leftover newline characters
+    while (t--)   // while testcases exist
+    {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.maxSubarraySum(arr) << endl;
+    }
+}
+// } Driver Code Ends
