1b52a36f5898b75e76d3425026daf8adf1f83fb8
Alok Kumar Raghuwanshi <143905515+Beast-Alok@users.noreply.github.com>
2024-09-18 00:10:52 +0530

    Create September_18th.cpp

---
+++ 09 September (2024)/September_18th.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to check if brackets are balanced or not.
+    bool ispar(string x)
+    {
+        // Your code here
+        stack<char>st ; 
+        for(int i=0;i<x.size();i++){
+            if(x[i] == '(' || x[i] == '[' || x[i] == '{'){
+                st.push(x[i]) ; 
+            }else{
+                if(st.empty()){
+                    return false ; 
+                }if(x[i] == ')' && st.top() == '('){
+                    st.pop() ; 
+                }else if(x[i] == ']' && st.top() == '['){
+                    st.pop() ; 
+                }else if(x[i] == '}' && st.top() == '{'){
+                    st.pop() ; 
+                }else{
+                    return false ; 
+                }
+            }
+        }if(st.empty()){
+            return true ;
+        }return false ; 
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+   int t;
+   string a;
+   cin>>t;
+   while(t--)
+   {
+       cin>>a;
+       Solution obj;
+       if(obj.ispar(a))
+        cout<<"balanced"<<endl;
+       else
+        cout<<"not balanced"<<endl;
+   }
+}
+// } Driver Code Ends
