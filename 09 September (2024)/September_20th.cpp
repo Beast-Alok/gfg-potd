9767f1d1ab971491501b42814d295d1889d4daa0
Alok Kumar Raghuwanshi <143905515+Beast-Alok@users.noreply.github.com>
2024-09-20 20:06:52 +0530

    Create September_20th.cpp

---
+++ 09 September (2024)/September_20th.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    // Returns count buildings that can see sunlight
+    int countBuildings(vector<int> &height) {
+        int maxi = height[0];
+        int count = 1;
+        for(auto x : height){
+            if(x > maxi){
+                count++;
+                maxi = max(maxi, x);
+            }
+        }
+        
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> height;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            height.push_back(number);
+        }
+        Solution ob;
+        int ans = ob.countBuildings(height);
+        cout << ans << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
