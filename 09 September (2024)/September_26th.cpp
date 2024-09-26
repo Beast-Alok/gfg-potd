e87ca600e98404868f4454ab1973b81b4a7754fb
Alok Kumar Raghuwanshi <143905515+Beast-Alok@users.noreply.github.com>
2024-09-27 00:01:05 +0530

    Create September_26th.cpp

---
+++ 09 September (2024)/September_26th.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to find maximum number of consecutive steps
+    // to gain an increase in altitude with each step.
+    int maxStep(vector<int>& arr) {
+        // Your code here
+        int count = 0;
+        int maxCount = 0;
+        for(int i = 0; i < arr.size()-1; i++){
+            if(arr[i+1] - arr[i] > 0){
+                count++;
+                // cout<<"p"<<endl;
+                maxCount = max(maxCount, count);
+            }else{
+                // cout<<"o"<<endl;
+                count = 0;
+            }
+        }
+        return maxCount;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        cout << ob.maxStep(arr) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
