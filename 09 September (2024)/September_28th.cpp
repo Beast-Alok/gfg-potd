c1a0328b49894490af837b3ec1920cab8a90b0a5
Alok Kumar Raghuwanshi <143905515+Beast-Alok@users.noreply.github.com>
2024-09-28 21:32:44 +0530

    Create September_28th.cpp

---
+++ 09 September (2024)/September_28th.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int minimizeCost(int k, vector<int>& arr) {
+        // Code here
+        int n = arr.size();
+        vector<int> dp(n,0);
+        for(int i=1;i<n;i++){
+            dp[i] = INT_MAX;
+            for(int j=max(0, i-k);j<i;j++){
+                dp[i] = min(dp[i], dp[j] + abs(arr[i]-arr[j]));
+            }
+        }
+        return dp[n-1];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int res = obj.minimizeCost(k, arr);
+        cout << res << endl;
+        // string tl;
+        // getline(cin, tl);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
