99310ebfdf9b2dda21a4ba848a16cc1baf7af141
Alok Kumar Raghuwanshi <143905515+Beast-Alok@users.noreply.github.com>
2024-09-29 22:27:20 +0530

    Create September_29th.cpp

---
+++ 09 September (2024)/September_29th.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int totalCount(int k, vector<int>& arr) {
+        // code here
+        int total = 0;
+        for(auto x : arr){
+            if(x%k == 0){
+                total += (x/k);
+            }else{
+                total += (x/k)+1;
+            }
+        }
+        return total;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int res = obj.totalCount(k, arr);
+        cout << res << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
