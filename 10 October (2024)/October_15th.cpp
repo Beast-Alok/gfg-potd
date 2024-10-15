fe9e8ebc1a62d564a69804c9eeb8757ddd44e05e
Alok Kumar Raghuwanshi <143905515+Beast-Alok@users.noreply.github.com>
2024-10-15 22:19:05 +0530

    Create October_15th.cpp

---
+++ 10 October (2024)/October_15th.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to count the number of subarrays which adds to the given sum.
+    int subArraySum(vector<int>& arr, int tar) {
+        int count = 0;
+        unordered_map<int, int> m;
+        int prefix_sum = 0;
+        for(auto x : arr){
+            prefix_sum += x;
+            m[prefix_sum]++;
+            
+            if(prefix_sum - tar == 0){
+                count++;
+            }else{
+                count += m[prefix_sum-tar];
+            }
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int tar = stoi(ks);
+        Solution obj;
+        int res = obj.subArraySum(arr, tar);
+        cout << res << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
